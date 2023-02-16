150282525ccf636d52064e2293a775a6df3365f8
rushabhshah27 <113459194+rushabhshah27@users.noreply.github.com>
2023-02-16 02:38:54 -0500

    update

---
+++ ThermometersMvc/Controllers/HomeController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ThermometersMvc.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ ThermometersMvc/Models/Thermometers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ThermometersMvc.Models
+{
+    public class Thermometers
+    {
+        public int Id { get; set; }
+        public string TypeOfThermometer { get; set; }
+        public decimal TemperatureRange { get; set; }
+        public string ProbeType { get; set; }
+        public string Accuracy { get; set; }
+        public decimal Price { get; set; }
+        public string Display { get; set; }
+    }
+}
