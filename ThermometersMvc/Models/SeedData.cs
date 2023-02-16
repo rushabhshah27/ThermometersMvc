2b8461cf0b7431259f20f0194a2f625272ff458e
rushabhshah27 <113459194+rushabhshah27@users.noreply.github.com>
2023-02-16 05:13:41 -0500

    About Us and SeedData Performed

---
+++ ThermometersMvc/Models/SeedData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ThermometersMvc.Data;
+using System;
+using System.Linq;
+
+namespace ThermometersMvc.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any movies.
+                if (context.Thermometers.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Thermometers.AddRange(
+                    new Thermometers
+                    {
+                        TypeOfThermometer = "Digital",
+                        TemperatureRange = 300,
+                        ProbeType = "Thermocouple",
+                        Accuracy = "0.1°C",
+                        Price = 10.99M,
+                        Display = "LCD"
+                    },
+                    new Thermometers
+                    {
+                        TypeOfThermometer = "Digital",
+                        TemperatureRange = 300,
+                        ProbeType = "Thermocouple",
+                        Accuracy = "0.1°C",
+                        Price = 10.99M,
+                        Display = "LCD"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
